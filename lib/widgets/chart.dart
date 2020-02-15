a341d80fae9934514ef578ab89f0c8f75ea67237
jonasreycian <cj.saraosos@gmail.com>
2020-02-16 04:21:39 +0800

    Finishing the chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -47,14 +47,22 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 3,
       margin: EdgeInsets.all(10),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return ChartBar(
-              data['day'], 
-              data['amount'], 
-              totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit : FlexFit.tight,
+              child: ChartBar(
+                  data['day'],
+                  data['amount'],
+                  totalSpending == 0.0
+                      ? 0.0
+                      : (data['amount'] as double) / totalSpending),
             );
-        }).toList(),
+          }).toList(),
+        ),
       ),
     );
   }
